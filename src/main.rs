3f6c3d3ae292d51bafbce4202e947664dd513c8d
aznos <aznosreal@gmail.com>
2026-02-02 18:41:15 -0500

    added some data structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Difficulty {
+    Easy,
+    Medium,
+    Hard,
+    Extreme
+}
+
+impl Difficulty {
+    fn as_str(&self) -> &'static str {
+        match self {
+            Difficulty::Easy => "easy",
+            Difficulty::Medium => "medium",
+            Difficulty::Hard => "hard",
+            Difficulty::Extreme => "extreme",
+        }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Challenge {
+    pub name: String,
+    pub difficulty: u8,
+    pub short_description: String,
+    pub description: String,
+    pub function_signature: String,
+    pub tests: Vec<TestCase>
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct TestCase {
+    pub input: Value,
+    pub expected: Value,
+}
+
 fn main() {
     println!("Hello world!");
 }
